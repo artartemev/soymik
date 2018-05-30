1310c324c2d911c3ede75407c30e8bb14a4e5a86
artartemev <79992180108@ya.ru>
2018-05-30 22:46:48 +0300

    again

--- server/sheet.js
+++ server/sheet.js
@@ -13,7 +13,7 @@ let oauth2Client;
 const users = {
     'veganov': {
         name: 'veganov',
-        spreadsheetId: '1WaEhstvY_h_2P-DYO2JIqeji2DXDL6UcF0tSOClyYDQ',
+        spreadsheetId: '1_dMcoa-DVtQDX4OJnCq2874EXGV_x5_nzZdyD3XTMFE',
         orderListRange: "Orders!A1",
         orderFieldsSequenceSheetId: 'Orders',
         goodsSheetId: 'Goods',
@@ -21,7 +21,7 @@ const users = {
     },
     'drzhbe': {
         name: 'drzhbe',
-        spreadsheetId: '1WaEhstvY_h_2P-DYO2JIqeji2DXDL6UcF0tSOClyYDQ',
+        spreadsheetId: '1kfKINyTGzsuycjz9O-uxXWwNf_e_DcuFTRwWWHjtxGE',
         orderListRange: 'Orders!A1',
         orderFieldsSequenceSheetId: 'Orders',
         goodsSheetId: 'Goods',
