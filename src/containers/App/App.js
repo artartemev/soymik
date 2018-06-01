1654fc447fd816f5226b1f20c8c049594679dbd9
artartemev <79992180108@ya.ru>
2018-06-01 17:21:35 +0300

    veganov

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -10,8 +10,8 @@ const App = ({sentSuccess, sentFail, closePopup}) => (
         <div className='info'>
             <div className='info__contacts'>
                 <img src={logo} alt='logo' width='200' height='100' className='info__logo' />
-                <p>Контакты: ‎‎‎‎‎8 952 388 5999 / zakazveganov@yandex.ru</p>
-                <p>Бухгалтерия: ‎‎‎‎8 900 649 5108 / veganovbuh@yandex.ru</p>
+                <p>Контакты: ‎‎‎‎‎8 952 388 5999 / zakazsoymik@yandex.ru</p>
+                <p>Бухгалтерия: ‎‎‎‎8 900 649 5108 / soymikbuh@yandex.ru</p>
             </div>
             <div className='info__content'>
                 <p>Заполните все поля формы.</p>
@@ -20,7 +20,7 @@ const App = ({sentSuccess, sentFail, closePopup}) => (
                 <p>Минимальный заказ - одна коробка в ассортименте.</p>
                 <p>После оформления заявки в течение дня бухгалтер выставит счёт по почте.</p>
                 <p>Груз отправляется в течение 1-3 рабочих дней после оплаты.</p>
-                <p>Срок годности колбас 45 суток, копчушки, сосисок и тофу 30 суток, подробности <a href="http://veganov.com/assortiment"> на сайте.</a> </p>
+                <p>Срок годности колбас 45 суток, копчушки, сосисок и тофу 30 суток, подробности <a href="http://soymik.com/assortiment"> на сайте.</a> </p>
             </div>
         </div>
         <OrderForm />
@@ -39,8 +39,8 @@ const App = ({sentSuccess, sentFail, closePopup}) => (
                     <div>
                         <h2>Что-то пошло не так!</h2>
                         <p>Позвоните нам и уточните как прошел заказ</p>
-                        <p>Контакты: 8 952 388 5999 / zakazveganov@yandex.ru</p>
-                        <p>Бухгалтерия: 8 900 649 5108 / veganovbuh@yandex.ru</p>
+                        <p>Контакты: 8 952 388 5999 / zakazsoymik@yandex.ru</p>
+                        <p>Бухгалтерия: 8 900 649 5108 / soymikbuh@yandex.ru</p>
                     </div>
                 }
                 <div className='popup__ok' onClick={closePopup}><span>x</span></div>
